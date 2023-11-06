ea35ab07a2ebf41d68f6a7fe172b0d4ac35f8172
Aurélien Ponte <aurelien.junk@gmail.com>
2023-11-06 12:27:51 +0100

    iterate over job_distributed code

--- sandbox/job_distributed.py
+++ sandbox/job_distributed.py
@@ -1,6 +1,6 @@
 import os, shutil
 import logging
-from time import time
+from time import sleep, time
 
 import numpy as np
 import pandas as pd
@@ -8,7 +8,8 @@ import xarray as xr
 from datetime import timedelta, datetime
 
 import dask
-from dask.delayed import delayed
+
+# from dask.delayed import delayed
 from dask.distributed import performance_report, wait
 from distributed.diagnostics import MemorySampler
 
@@ -22,39 +23,99 @@ root_dir = "/home/datawork-lops-osi/equinox/mit4320/parcels/"
 run_name = "global_dij8_up3_r2s111_30j_201202"
 
 # will overwrite existing results
-#overwrite = True
+# overwrite = True
 overwrite = False
 
 # dask parameters
-dask_jobs = 8
-jobqueuekw = dict(processes=2, cores=2) # uplet debug
+dask_jobs = 2  # number of dask pbd jobs
+jobqueuekw = dict(processes=2, cores=2)  # uplet debug
 
 
-# ---------------------------------- dask utils ---------------------------------- 
-
-def trim_memory() -> int:
-    libc = ctypes.CDLL("libc.so.6")
-    return libc.malloc_trim(0)
+# ---------------------------------- dask utils ----------------------------------
 
 
-def spin_up_cluster(jobs):
-    logging.info("start spinning up dask cluster, jobs={}".format(jobs))
-    cluster, client = ut.spin_up_cluster(
-        "distributed",
-        jobs=jobs,
-        fraction=0.9,
-        walltime="12:00:00",
-        **jobqueuekw,
-    )
-    
-    logging.info("dashboard via ssh: " + ut.dashboard_ssh_forward(client))
+def spin_up_cluster(
+    ctype,
+    jobs=None,
+    processes=None,
+    fraction=0.8,
+    timeout=20,
+    **kwargs,
+):
+    """Spin up a dask cluster ... or not
+    Waits for workers to be up for distributed ones
+
+    Paramaters
+    ----------
+    ctype: None, str
+        Type of cluster: None=no cluster, "local", "distributed"
+    jobs: int, optional
+        Number of PBS jobs
+    processes: int, optional
+        Number of processes per job (overrides default in .config/dask/jobqueue.yml)
+    timeout: int
+        Timeout in minutes is cluster does not spins up.
+        Default is 20 minutes
+    fraction: float, optional
+        Waits for fraction of workers to be up
+
+    """
+
+    if ctype is None:
+        return
+    elif ctype == "local":
+        from dask.distributed import Client, LocalCluster
+
+        dkwargs = dict(n_workers=14, threads_per_worker=1)
+        dkwargs.update(**kwargs)
+        cluster = LocalCluster(**dkwargs)  # these may not be hardcoded
+        client = Client(cluster)
+    elif ctype == "distributed":
+        from dask_jobqueue import PBSCluster
+        from dask.distributed import Client
+
+        assert jobs, "you need to specify a number of dask-queue jobs"
+        cluster = PBSCluster(processes=processes, **kwargs)
+        cluster.scale(jobs=jobs)
+        client = Client(cluster)
+
+        if not processes:
+            processes = cluster.worker_spec[0]["options"]["processes"]
+
+        flag = True
+        start = time()
+        while flag:
+            wk = client.scheduler_info()["workers"]
+            print("Number of workers up = {}".format(len(wk)))
+            sleep(5)
+            if len(wk) >= processes * jobs * fraction:
+                flag = False
+                print("Cluster is up, proceeding with computations")
+            now = time()
+            if (now - start) / 60 > timeout:
+                flag = False
+                print("Timeout: cluster did not spin up, closing")
+                cluster.close()
+                client.close()
+                cluster, client = None, None
+
     return cluster, client
+
+
+def dashboard_ssh_forward(client):
+    """returns the command to execute on a local computer in order to
+    have access to dashboard at the following address in a browser:
+    http://localhost:8787/status
+    """
+    env = os.environ
+    port = client.scheduler_info()["services"]["dashboard"]
+    return f'ssh -N -L {port}:{env["HOSTNAME"]}:8787 {env["USER"]}@datarmor1-10g'
 
 
 def close_dask(cluster, client):
     logging.info("starts closing dask cluster ...")
     try:
-        
+
         client.close()
         logging.info("client closed ...")
         # manually kill pbs jobs
@@ -80,9 +141,15 @@ def manual_kill_jobs():
     #
     bashCommand = "qstat"
     try:
-        output = subprocess.check_output(bashCommand, shell=True, stderr=subprocess.STDOUT)
+        output = subprocess.check_output(
+            bashCommand, shell=True, stderr=subprocess.STDOUT
+        )
     except subprocess.CalledProcessError as e:
-        raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+        raise RuntimeError(
+            "command '{}' return with error (code {}): {}".format(
+                e.cmd, e.returncode, e.output
+            )
+        )
     #
     for line in output.splitlines():
         lined = line.decode("UTF-8")
@@ -96,51 +163,53 @@ def manual_kill_jobs():
                 # print(e.output.decode())
                 pass
 
+
 def dask_compute_batch(computations, client, batch_size=None):
-    """ breaks down a list of computations into batches
-    """
+    """breaks down a list of computations into batches"""
     # compute batch size according to number of workers
     if batch_size is None:
         # batch_size = len(client.scheduler_info()["workers"])
         batch_size = sum(list(client.nthreads().values()))
     # find batch indices
     total_range = range(len(computations))
-    splits = max(1, np.ceil(len(total_range)/batch_size))
+    splits = max(1, np.ceil(len(total_range) / batch_size))
     batches = np.array_split(total_range, splits)
     # launch computations
     outputs = []
     for b in batches:
-        logging.info("batches: " + str(b)+ " / "+str(total_range))
-        out = dask.compute(*computations[slice(b[0], b[-1]+1)])
+        logging.info("batches: " + str(b) + " / " + str(total_range))
+        out = dask.compute(*computations[slice(b[0], b[-1] + 1)])
         outputs.append(out)
-        
+
         # try to manually clean up memory
         # https://coiled.io/blog/tackling-unmanaged-memory-with-dask/
         client.run(gc.collect)
         client.run(trim_memory)  # should not be done systematically
-        
+
     return sum(outputs, ())
 
 
+def trim_memory() -> int:
+    libc = ctypes.CDLL("libc.so.6")
+    return libc.malloc_trim(0)
 
-# ---------------------------------- core of the job to be done ---------------------------------- 
+
+# ---------------------------------- core of the job to be done ----------------------------------
+
 
 def run(v, cluster, client):
     """ """
 
-    # load dataset
+    # load or create dataset and do work
     # ds = ...
+    n, c = 10_000, 1_000
+    x = dask.array.random.random((n, n), chunks=(c, c))
+    y = dask.array.random.random((n, n), chunks=(c, c))
+    z = dask.array.where((x**2 + y**2) < 1, 1, 0)
+    result = 4 * z.sum().compute() / n**2
 
-    # set start and end times
-    #t_end = t_start + int(T * 86400 / dt_window.total_seconds()) * dt_window
-    #str_fmt = "Global start = {}  /  Global end = {}".format(
-    #    t_start.strftime("%Y-%m-%d %H:%M"),
-    #    t_end.strftime("%Y-%m-%d %H:%M"),
-    #)
-    str_fmt = "some information in run"
+    str_fmt = f"some information in run ... result={result}"
     logging.info(str_fmt)
-
-    # do work
 
     flat_out = True
     return flag_out
@@ -154,7 +223,7 @@ if __name__ == "__main__":
     logging.basicConfig(
         filename="distributed.log",
         level=logging.INFO,
-        #level=logging.DEBUG,
+        # level=logging.DEBUG,
     )
     # level order is: DEBUG, INFO, WARNING, ERROR
     # encoding='utf-8', # available only in latests python versions
@@ -166,7 +235,7 @@ if __name__ == "__main__":
     # create run directory tree
     logging.info("1 - step 1")
     # ...
-    
+
     # create tiling
     logging.info("2 - step 2")
     # ...
@@ -179,19 +248,36 @@ if __name__ == "__main__":
 
         logging.info("--- reboot %d", reboot)
 
-        # sping up cluster
-        cluster, client = spin_up_cluster(dask_jobs)
+        # spin up cluster
+        logging.info("start spinning up dask cluster, jobs={}".format(dask_jobs))
+        cluster, client = spin_up_cluster(
+            "distributed",
+            jobs=dask_jobs,
+            fraction=0.9,
+            walltime="12:00:00",
+            **jobqueuekw,
+        )
+        logging.info("dashboard via ssh: " + dashboard_ssh_forward(client))
         print(cluster)
 
-        # run parcels simulation
-        ms = MemorySampler()
-        with performance_report(filename=f"dask-report-{reboot}.html"), ms.sample(f"reboot{reboot}"):
-            flag = run(None, cluster, client)
-        # https://distributed.dask.org/en/latest/diagnosing-performance.html#analysing-memory-usage-over-time
-        ms.to_pandas().to_csv(f"dask-memory-report-{reboot}.csv")
+        # run case WITHOUT MemorySampler:
+        flag = run(None, cluster, client)
+
+        # run case WITH MemorySampler:
+        # ms = MemorySampler()
+        # with performance_report(filename=f"dask-report-{reboot}.html"), ms.sample(
+        #    f"reboot{reboot}"
+        # ):
+        #    flag = run(None, cluster, client)
+        ## https://distributed.dask.org/en/latest/diagnosing-performance.html#analysing-memory-usage-over-time
+        # ms.to_pandas().to_csv(f"dask-memory-report-{reboot}.csv")
 
         # close dask
         close_dask(cluster, client)
+
+        # mechanism to exit (here or in run via flag)
+        if reboot > 5:
+            flag == False
 
         reboot += 1
 
